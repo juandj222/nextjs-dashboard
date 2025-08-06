b96a93f2cd03db6d4eae36095fc605b439ce9811
Juan django <djangojuan19@gmail.com>
2025-08-06 12:34:46 +0200

    Correction navigation et ajout du tableau links

--- facture/page.tsx
+++ facture/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import NavLinks from '@/app/ui/nav-links';
+import NavLinks from '@/app/ui/dashboard/nav-links';
 
 export default function facture() {
   <NavLinks />
